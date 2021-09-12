798974d0ae0e2b294015a80eb4024c333fc293da
mohitjayant <moxito@MBRLT00994.timesgroup.com>
2021-09-13 05:27:11 +0530

    balanced String

---
+++ balancedString.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ld long double
+
+#define vt vector
+#define pb push_back
+
+const ll INF = 2e18;
+const int maxm= 2e5+5;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    cout<<"testing";
+}
